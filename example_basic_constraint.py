e5cb6a13946a2ac2d0338e6e630d2cad552094b3
Mikael Honkala <mikael.honkala@gmail.com>
2023-05-10 21:01:17 +0300

    Conditions example

--- example_basic_constraint.py
+++ example_basic_constraint.py
@@ -7,9 +7,10 @@ def main(page: ft.Page):
     center = a.Anchored(ft.FilledButton("Center"), dock_center=root)
     on_the_side = a.Anchored(ft.FilledButton("On the side"), top=center.top, left=center.right)
     one_quarter_from_the_top = a.Anchored(ft.FilledButton("1/4 height"), center_x=root.center_x, center_y=root.height / 4)
-    max_position = a.Anchored(ft.FilledButton("Max position"), center_y=root.height * 3/4, center_x=max(root.center_x, 300))
-    # reactive = a.Anchored(ft.FilledButton("Reactive"), center_x=(root.width >= 500) & root.width / 2 | 200)
-    root.controls = [center, on_the_side, one_quarter_from_the_top, max_position]
+    adjusted = a.Anchored(ft.FilledButton("Adjusted"), center_x=root.width * 3/4, center_y=20 + root.height / 4)
+    max_position = a.Anchored(ft.FilledButton("Max position"), center_y=root.height * 3/4, center_x=max(300, root.center_x))
+    reactive = a.Anchored(ft.FilledButton("Reactive"), center_x=(root.width >= 600) & root.width / 2)
+    root.controls = [center, on_the_side, one_quarter_from_the_top, adjusted, max_position, reactive]
 
 
 ft.app(main)
